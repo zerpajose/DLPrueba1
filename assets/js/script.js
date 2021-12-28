4217f460161e00be9170c400e967ebce6dcfc735
Jose Zerpa <zjose88@gmail.com>
2021-12-28 01:31:30 -0300

    fourth commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+/* Parta que funcione el tooltip de bootstrap */
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+});
+
